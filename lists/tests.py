5e1e9b0d866b6198aa466fb2c593cd33826ffe1b
ABo213 <120735948@qq.com>
2017-10-18 22:13:41 -0700

    Make functional test check multiple items

--- lists/tests.py
+++ lists/tests.py
@@ -9,3 +9,9 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_post_request(self):
+        item_text = 'A new list item'
+        response = self.client.post('/', data={'item_text': item_text})
+        self.assertIn(item_text, response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
