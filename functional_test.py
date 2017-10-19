eb5713aa725a054b8ee33a88de814c8c9a841df8
ABo213 <120735948@qq.com>
2017-10-18 18:25:57 -0700

    Add functional test for adding to-do items

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common import keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,6 +15,21 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(keys.Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
         self.fail('Finish the test!')
 
 
