89e42956302ac35a635426155e89c5abff1b63e6
ABo213 <120735948@qq.com>
2017-11-08 08:12:48 -0800

    Add FT for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,7 +16,7 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_one_user_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
@@ -37,6 +37,36 @@ class NewVisitorTest(LiveServerTestCase):
         self._wait_and_check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
+
+    def test_multiple_user_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('new_item')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(keys.Keys.ENTER)
+        self._wait_and_check_for_row_in_list_table('1: Buy peacock feathers')
+
+        ones_list_url = self.browser.current_url
+        self.assertRegex(ones_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(keys.Keys.ENTER)
+        self._wait_and_check_for_row_in_list_table('1: Buy milk')
+
+        others_list_url = self.browser.current_url
+        self.assertRegex(others_list_url, '/lists/.+')
+        self.assertNotEqual(others_list_url, ones_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
     def _wait_and_check_for_row_in_list_table(self, row_text):
         start_time = time.time()
