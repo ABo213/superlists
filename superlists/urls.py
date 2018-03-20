813b93ad16a8fab193876f07a451534e1415ebe8
ABo213 <120735948@qq.com>
2018-03-20 07:33:50 -0700

    Refactor urls.

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include
 
-import lists.views
+from lists import views as lists_views
+from lists import urls as lists_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', lists.views.home_page, name='home'),
-    url(r'^lists/new$', lists.views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', lists.views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', lists.views.add_item, name='add_item')
+    url(r'^$', lists_views.home_page, name='home'),
+    url(r'lists/', include(lists_urls))
 ]
