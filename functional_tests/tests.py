482f7566c54ac79b41c42b813a3aeef117b8780f
ABo213 <120735948@qq.com>
2018-03-20 08:05:22 -0700

    Refactor templates.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -65,6 +65,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        input_box = self.browser.find_element_by_id('new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2, 512, delta=10)
+
+        input_box.send_keys('testing')
+        input_box.send_keys(keys.Keys.ENTER)
+        self._wait_and_check_for_row_in_list_table('1: testing')
+        input_box = self.browser.find_element_by_id('new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def _wait_and_check_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
