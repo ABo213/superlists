4300ec385cf0e4242e375e87dd6e741949c84917
ABo213 <120735948@qq.com>
2018-03-25 09:38:09 -0700

    Add a form for the List model

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,24 +1,10 @@
-import os
-import time
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common import keys
 
-MAX_WAIT_SECOND = 10
+from functional_tests.base import FunctionalTest
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Chrome()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
+class NewVisitorTest(FunctionalTest):
     def test_one_user_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -26,14 +12,14 @@ class NewVisitorTest(StaticLiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        input_box = self.browser.find_element_by_id('new_item')
+        input_box = self.browser.find_element_by_id('id_text')
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(keys.Keys.ENTER)
         self._wait_and_check_for_row_in_list_table('1: Buy peacock feathers')
 
-        input_box = self.browser.find_element_by_id('new_item')
+        input_box = self.browser.find_element_by_id('id_text')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(keys.Keys.ENTER)
         self._wait_and_check_for_row_in_list_table('1: Buy peacock feathers')
@@ -41,7 +27,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_multiple_user_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
-        input_box = self.browser.find_element_by_id('new_item')
+        input_box = self.browser.find_element_by_id('id_text')
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(keys.Keys.ENTER)
         self._wait_and_check_for_row_in_list_table('1: Buy peacock feathers')
@@ -56,7 +42,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
 
-        input_box = self.browser.find_element_by_id('new_item')
+        input_box = self.browser.find_element_by_id('id_text')
         input_box.send_keys('Buy milk')
         input_box.send_keys(keys.Keys.ENTER)
         self._wait_and_check_for_row_in_list_table('1: Buy milk')
@@ -69,31 +55,42 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+
+class LayoutAndStylingTest(FunctionalTest):
     def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(480, 360)
-        input_box = self.browser.find_element_by_id('new_item')
+        input_box = self.browser.find_element_by_id('id_text')
         self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2, 240, delta=10)
 
         input_box.send_keys('testing')
         input_box.send_keys(keys.Keys.ENTER)
         self._wait_and_check_for_row_in_list_table('1: testing')
-        input_box = self.browser.find_element_by_id('new_item')
+        input_box = self.browser.find_element_by_id('id_text')
         self.assertAlmostEqual(
             input_box.location['x'] + input_box.size['width'] / 2,
             240,
             delta=10
         )
 
-    def _wait_and_check_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() > start_time + MAX_WAIT_SECOND:
-                    raise e
-                time.sleep(0.5)
+
+class ItemValidationTest(FunctionalTest):
+    def test_cannot_add_emtpy_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys(keys.Keys.ENTER)
+
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
+
+        self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
+        self.get_item_input_box().send_keys(keys.Keys.ENTER)
+        self._wait_and_check_for_row_in_list_table('1: Buy milk')
+
+        self.get_item_input_box().send_keys(keys.Keys.ENTER)
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:invalid'))
+
+        self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector('#id_text:valid'))
+        self.get_item_input_box().send_keys(keys.Keys.ENTER)
+        self._wait_and_check_for_row_in_list_table('1: Buy milk')
+        self._wait_and_check_for_row_in_list_table('2: Make tea')
---
+++ lists/tests/test_views.py
@@ -0,0 +1,123 @@
+from django.test import TestCase
+from django.utils.html import escape
+
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR_MESSAGE
+from lists.models import Item, List
+
+
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+
+class NewListTest(TestCase):
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR_MESSAGE))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+
+class ListViewTest(TestCase):
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
+
+    def test_can_save_a_post_request(self):
+        item_text = 'A new list item'
+        response = self.client.post('/lists/new', data={'text': item_text})
+
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_text)
+
+    def test_redirects_after_post(self):
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
+        list_ = List.objects.first()
+        self.assertRedirects(response, f'/lists/{list_.id}/')
+
+    def test_can_save_a_post_request_to_an_existing_list(self):
+        list_ = List.objects.create()
+        another_list = List.objects.create()
+
+        self.client.post(f'/lists/{list_.id}/', data={'text': 'A new item for an existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        item = Item.objects.first()
+        self.assertEqual(item.text, 'A new item for an existing list')
+        self.assertEqual(item.list, list_)
+
+    def test_post_redirects_to_list_view(self):
+        list_ = List.objects.create()
+        another_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{list_.id}/',
+                                    data={'text': 'A new item for an existing list'})
+
+        self.assertRedirects(response, f'/lists/{list_.id}/')
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR_MESSAGE))
+
+    def test_invalid_list_items_not_saved(self):
+        self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_only_items_for_that_list(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+        another_list = List.objects.create()
+        Item.objects.create(text='other itemey 1', list=another_list)
+        Item.objects.create(text='other itemey 2', list=another_list)
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other itemey 1')
+        self.assertNotContains(response, 'other itemey 2')
+
+    def test_passes_correct_list_to_template(self):
+        list_ = List.objects.create()
+        other_list = List.objects.create()
+
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertEqual(response.context['list'], list_)
+
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(f'/lists/{list_.id}/', data={'text': ''})
